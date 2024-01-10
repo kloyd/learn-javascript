ee06dfeabc03bc70ba3852cf7b0b251b477b6a9c
kloyd <kelly.loyd@icloud.com>
2024-01-10 16:48:17 -0500

    scoped variables

---
+++ Scope/scope_lab.js
@@ -0,0 +1,32 @@
+// Global
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global but scoped with let";
+const globalConst = "I'm a global constant";
+
+{
+    // Block scope
+    var blockVar = "I'm a block scoped variable";
+    let blockLet = "I'm a block scoped let";
+    const blockConst = "I'm a block scoped const";
+}
+
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
+
+//console.log(blockVar);
+//console.log(blockLet);
+
+
+function show() {
+    var functionVar = "I'm a function var";
+    let functionLet = "I'm a block scoped let";
+    const functionConst = "I'm a block scoped const";
+
+}
+
+show();
+
+console.log(functionVar);
+console.log(functionLet);
+console.log(functionConst);
