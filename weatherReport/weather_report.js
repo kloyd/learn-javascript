0d6c90d576e40ee01fe429a30c9b558c3bfcff81
Kelly Loyd <kelly.loyd@icloud.com>
2024-01-23 14:11:31 -0500

    code in right place

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -2,24 +2,24 @@ const apiKey = 'fa55682f6596cbf56987304b0bdd8780'; // Replace 'YOUR_API_KEY' wit
 let city = "";
 let apiUrl = "";
 function showweatherDetails(event) {
+    event.preventDefault();
+ 
     city = document.getElementById('city').value;
     apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    event.preventDefault();
-}
-
-
-fetch(apiUrl)
-.then(response => response.json())
-.then(data => {
-  const weatherInfo = document.getElementById('weatherInfo');
-  weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
                           <p>Temperature: ${data.main.temp} &#8451;</p>
                           <p>Weather: ${data.weather[0].description}</p>`;
-})
-.catch(error => {
-    console.error('Error fetching weather:', error);
-    const weatherInfo = document.getElementById('weatherInfo');
-    weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
-});
+        })
+        .catch(error => {
+           console.error('Error fetching weather:', error);
+         const weatherInfo = document.getElementById('weatherInfo');
+         weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        });
+}
+
 
 document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
