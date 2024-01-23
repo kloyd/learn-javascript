088b1281ec7be1a06e5dfff176f2b78aebf79db3
Kelly Loyd <kelly.loyd@icloud.com>
2024-01-23 14:49:22 -0500

    lat/lon search

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -21,5 +21,27 @@ function showweatherDetails(event) {
         });
 }
 
+function showeatherByCoords(event) {
+    event.preventDefault();
+ 
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+
+    apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherByCoords');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                          <p>Temperature: ${data.main.temp} &#8451;</p>
+                          <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+           console.error('Error fetching weather:', error);
+         const weatherInfo = document.getElementById('weatherByCoords');
+         weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        });
+}
 
 document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
+document.getElementById('weatherFormByCoords').addEventListener('submit', showeatherByCoords);
